d73e2d0c56eaf2b8579992b2fe5ed99710324ac1
Lyudmila Khan <lud853@mail.ru>
2025-10-21 23:08:27 +0300

    first commit book-reader

---
+++ dom/book-reader/task.js
@@ -0,0 +1,19 @@
+const book = document.getElementById('book');
+const fontControls = document.querySelectorAll('.font-size');
+
+fontControls.forEach(control => {
+    control.addEventListener('click', event => {
+        event.preventDefault();
+    
+        fontControls.forEach(item => item.classList.remove('font-size_active'));
+        control.classList.add('font-size_active');
+        book.classList.remove('book_fs-small', 'book_fs-big');
+
+        const size = control.dataset.size;
+        if (size === 'small') {
+            book.classList.add('book_fs-small');
+        } else if (size === 'big') {
+            book.classList.add('book_fs-big');
+        }
+    });
+});
